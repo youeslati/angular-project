fede71bdcb629e54dd6cecb22c93bceb0007fe59
yassine <yassine.oueslati.inf@gmail.com>
2021-03-16 11:11:47 +0100

    de

--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -17,8 +17,8 @@ export class ProduitsComponent implements OnInit {
   getProduits(){
     this.produits= [new Produits('Clavier','un clavier de gaming de haute qualité','https://cours-informatique-gratuit.fr/wp-content/uploads/2014/05/clavier-1.jpg',50)
   , new Produits('Ordinateur','i7 10 Géneration 1024 GO 16G RAM','https://www.technopro-online.com/22399-large_default/prix-pc-de-bureau-lenovo-s510-dual-core-4go-tunisie.jpg',1300)
-  ]
-    return this.produits;
+
+    ]
   }
 
 fermerOuvrir(){
